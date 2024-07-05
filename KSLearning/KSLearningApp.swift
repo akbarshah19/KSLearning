b3acb0c9490a14a21c02b287978e437f6daee79f
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-07-05 09:28:48 +0500

    Initial Commit

---
+++ KSLearning/KSLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  KSLearningApp.swift
+//  KSLearning
+//
+//  Created by Akbarshah Jumanazarov on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KSLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

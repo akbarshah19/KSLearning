154ddb6a406a400fb67d991c490372d4244cb53b
akbarshah19 <akbarshah19@icloud.com>
2025-02-05 23:42:48 +0500

    MultipleImageViewer init

---
+++ KSLearning/MultipleImageViewer/MultipleImageViewer.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleImageViewer.swift
+//  KSLearning
+//
+//  Created by Akbarshah Jumanazarov on 2/5/25.
+//
+
+import SwiftUI
+
+struct MultipleImageViewer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleImageViewer()
+}
